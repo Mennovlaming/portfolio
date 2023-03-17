a95a93d5e80ea6fe452952953c0479b761a6426e
Menno Vlaming <mennovlaming@re-byods-145-28-146-116.wireless.hva.nl>
2023-03-17 11:58:54 +0100

    contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,6 +4,22 @@ function Contact() {
     return(
         <>
             <h2>Contact</h2>
+            <form action="">
+                <label>Name
+                <input type="text" />
+                </label>
+
+                <label>E-mail
+                <input type="text" />
+                </label>
+
+               
+
+                <label>Message
+                <textarea>Enter text here...</textarea>
+                </label>
+                <button>send</button>
+            </form>
         </>
     )
 }
