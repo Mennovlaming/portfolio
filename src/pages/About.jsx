f9e20c1f58de0cb48a6f43a76f07a20a02323dbc
Menno Vlaming <mennovlaming@mbp-van-menno.kpn>
2023-04-17 16:20:04 +0200

    build

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,8 +6,8 @@ function About() {
         <section className="about">
             <h2>About me</h2>
             <p>My name is Menno Vlaming, I'm a front-end developer/UX designer from The Netherlands. 
-                I am currently studying CMD at the Amsterdam university of applied science. <br />
-                I started this study as a UX designer. <br />And troughout the study i got more into coding, I started to
+                I am currently studying CMD at the Amsterdam university of applied sciences. <br />
+                I started this study as a UX designer. <br />And trhoughout the study I got more into coding, I started to
                 like it! <br /><br />So now my goal is to learn more about code en develop more skills en knowledge, because that's 
                 the beautifull thing about coding, you are never done learning.
             </p>
